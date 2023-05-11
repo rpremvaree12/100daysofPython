2475c48bd6baf59907ce3660fc38e2a9c41b761b
rpremvaree12 <richard.premvaree@gmail.com>
2023-05-10 22:05:42 -0400

    basic game of blackjack, no double down yet

--- 011blackjack/main.py
+++ 011blackjack/main.py
@@ -3,7 +3,7 @@ import random
 
 '''
 Blackjack - unlimited cards, only 1 player vs. dealer.
-1. deal 2 cards to each person. only shows first card of dealer.
+1. deal 2 cards to each person. only show first card of dealer.
 2. give option to hit or stay.
 3. if above 21, player loses    
 4. if stays, dealer shows second card. if less than 17, has to hit.
@@ -17,64 +17,109 @@ player_cards = []
 dealer_cards = []
 bankroll = 1000
 
-def dealToPlayer(action):
-    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-    if action =="stay":
-        return
-    elif action == "hit":
-        player_cards.append(random.choice(cards))
-        return
-    elif action == "double":
-        player_cards.append(random.choice(cards))
-        return
-
 def gameStart():
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     for n in range(2):
         player_cards.append(random.choice(cards))
         dealer_cards.append(random.choice(cards))
+    displayHand(player_cards)
+    print(f"\tDealer's first card is: {dealer_cards[0]}.\n")
     return
+
+def dealToPlayer(action):
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    if action == "hit" or action == "double":
+        player_cards.append(random.choice(cards))
+        displayHand(player_cards)
+        return
+    
+def dealToDealer():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    print("Dealer's turn.\n")
+    while handTotal(dealer_cards) <= 16:
+        print("Dealer hits.\n")
+        dealer_cards.append(random.choice(cards))
+        displayHand(dealer_cards)
+    if handTotal(dealer_cards) > 21:
+        print("Dealer busts.\n")
+        return True
+    else:
+        print(f"dealer: {handTotal(dealer_cards)} player: {handTotal(player_cards)}")
+        return handTotal(dealer_cards) < handTotal(player_cards)
 
 def displayHand(cards):
     if cards == player_cards:
-        print(f"\tYour cards are {cards}. Total: {handTotal(player_cards)}")
-    if cards == dealer_cards and action == "stay":
-        print(f"\tYour cards are {cards}. Total: {handTotal(player_cards)}")
-
+        print(f"\tYour cards: {cards}.\n\tYou have: {handTotal(player_cards)}\n")
+        return
+    elif cards == dealer_cards:
+        print(f"\tDealer's cards are {cards},\n \tDealer has: {handTotal(dealer_cards)}\n")
+        return
+    else:
+        return
 
 def handTotal(hand):
-    handTotal = 0
+    total = 0
     for c in hand:
-        handTotal += c
-    return handTotal
+        total += c
+    if 11 in hand and total > 21:
+        total -= 10
+    return total
+    
 
+# game start section
 print(art.logo)
 print("Welcome to Python BlackJack.")
 play = input("Would you like to play? 'y' or 'n'?\n")
 if play.lower()[0] == 'y':
-    bet = int(input("Place your bet.\n$"))
+    bet = int(input(f"Your current bankroll is ${bankroll}. Place your bet.\n$"))
+    gameStart()
 
-    print(dealToPlayer("deal"))
-    dealToDealer("deal")
+player_end = False
+while not player_end:
 
-print(f"\tDealer's first card is: {dealer_cards[0]}")
+    ##check if player has blackjack - add to 21 and 2 cards
+    if handTotal(player_cards) == 21 and len(player_cards) == 2:
+        if handTotal(dealer_cards) == 21:
+            print("Push! Dealer has Blackjack.")
+            displayHand(dealer_cards)
 
-game_end = False
-while not game_end:
-    if handTotal(player_cards) == 21:
-        print("Blackjack!")
-        bankroll += 1.5 * bet
+        else:
+            bankroll += 1.5 * bet
+            player_end = True
+            print("Blackjack!")
+            print(f"Your bankroll is now ${bankroll}.")
+
+    #player does not have black jack        
     else:
+        
         action = input("Would you like to hit, stay or double?\n")
-    dealToPlayer(action)
-    displayPlayerHand()
+        if action == "hit":
+            dealToPlayer("hit")
 
+        elif action == "stay":
+            player_end = True
 
-    if handTotal(player_cards) > 21:
-        print("Bust! You lose.")
-        game_end = True
-        bankroll -= bet
-    elif handTotal(dealer_cards) > 21 or handTotal(player_cards) > handTotal(dealer_cards):
-        game_end = True
-        print("You win.")
-        bankroll += bet
+            #deal to the dealer
+            if dealToDealer():
+                print("You win.")
+
+            else:
+                print("You lose.")
+
+        # elif action == "double":
+        #     dealToPlayer("double")
+        #     bet *= 2
+        #     player_end = True
+            
+        #     #deal to the dealer
+        #     if dealToDealer():
+        #         print("You win.")
+
+        #     else:
+        #         print("You lose.")
+
+        if handTotal(player_cards) > 21:
+            displayHand(dealer_cards)
+            print("Bust! You lose.\n")
+            bankroll -= bet
+            player_end = True
