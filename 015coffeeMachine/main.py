47de8c0cb8ba3f7c59527ff12aede7a2b011cf0e
Richard <richard.premvaree@gmail.com>
2023-05-16 14:53:28 -0400

    some progress

--- 015coffeeMachine/main.py
+++ 015coffeeMachine/main.py
@@ -5,7 +5,52 @@
 # process coins
 # check if transaction is successful
 # make coffee - deduct resources
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
 
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+    "money": 0
+}
 
-def makeCoffee:
+def report():
+    for item in resources: 
+        if item == "water" or item == "milk" or item == "coffee":
+            print(f"{item}: {resources[item]} ml")
     return
+
+def process_coins():
+    return
+
+def check_resources(bev):
+    return True
+
+def make_coffee():
+    return
+
+report()
