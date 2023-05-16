5434cd52915e29b25afb23657d142f817e56837a
Richard <richard.premvaree@gmail.com>
2023-05-16 14:06:28 -0400

    program requirements

---
+++ 015coffeeMachine/main.py
@@ -0,0 +1,11 @@
+# TODOs
+# print out a report of water, milk, coffee and money by entering "report"
+# turn machine off by entering "off"
+# check if resources are sufficient and produces change
+# process coins
+# check if transaction is successful
+# make coffee - deduct resources
+
+
+def makeCoffee:
+    return
