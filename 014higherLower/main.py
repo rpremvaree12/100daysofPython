a37709cac8020b1de613de7f76f71fd8099b6e5d
Richard <richard.premvaree@gmail.com>
2023-05-16 12:11:15 -0400

    added a scoreboard feature

--- 014higherLower/main.py
+++ 014higherLower/main.py
@@ -1,37 +1,58 @@
 import random
+import json
 import art
-from game_data import data
+from game_data import data as game_data
 
+with open("db.json","r") as database:
+    db = json.load(database)
+    high_scores ={
+    "score": db["score"],
+    "name": db["name"]
+    }
+database.close()
 
 score = 0
 should_continue = True
 
 while should_continue:
-    opt_a = data[random.choice(range(1,len(data)))]
-    opt_b = data[random.choice(range(1,len(data)))]
+    opt_a = game_data[random.choice(range(1,len(game_data)))]
+    opt_b = game_data[random.choice(range(1,len(game_data)))]
 
+    #check if the chosen are the same.
+    if opt_a['name'] != opt_b['name']:
 
-    print(f"Compare A: {opt_a['name']}, a {opt_a['description']}, from {opt_a['country']}")
-    print(art.vs)
-    print(f"Against B: {opt_b['name']}, a {opt_b['description']}, from {opt_b['country']}")
-
-    choice = input("Which Instagram account has more followers? Type 'A' or 'B' ").upper()
-    print(chr(27) + "[2J")
-
-    if choice == "A" and opt_a['follower_count'] > opt_b['follower_count']:
-        score += 1
-        should_continue = True
         print(art.logo)
-        print(f"You're right! Current score: {score}")
-    
-    elif choice == "B" and opt_a['follower_count'] < opt_b['follower_count']:
-        score += 1
-        should_continue = True
-        print(art.logo)
-        print(f"You're right! Current score: {score}")
+        print(f"Compare A: {opt_a['name']}, a {opt_a['description']}, from {opt_a['country']}")
+        print(art.vs)
+        print(f"Against B: {opt_b['name']}, a {opt_b['description']}, from {opt_b['country']}")
 
-    else:
-        should_continue = False
-        print(art.logo)
-        print(f"Sorry. That's wrong.\n{opt_a['name']} has {opt_a['follower_count']} followers and \n{opt_b['name']} has {opt_b['follower_count']} followers\n")
-        print(f"Final score: {score}\n")
+        choice = input("Which Instagram account has more followers? Type 'A' or 'B' ").upper()
+        
+        #clear the screen
+        print(chr(27) + "[2J")
+
+        if choice == "A" and opt_a['follower_count'] > opt_b['follower_count']:
+            score += 1
+            should_continue = True
+            print(f"You're Right! {opt_a['name']} has {opt_a['follower_count']},000 followers and \n{opt_b['name']} has {opt_b['follower_count']},000 followers\n")
+            print(f"You're right! Current score: {score}")
+        
+        elif choice == "B" and opt_a['follower_count'] < opt_b['follower_count']:
+            score += 1
+            should_continue = True
+            print(f"You're Right! {opt_a['name']} has {opt_a['follower_count']},000 followers and \n{opt_b['name']} has {opt_b['follower_count']},000 followers\n")
+            print(f"You're right! Current score: {score}")
+
+        else:
+            should_continue = False
+            print(f"Sorry. That's wrong.\n{opt_a['name']} has {opt_a['follower_count']},000 followers and \n{opt_b['name']} has {opt_b['follower_count']},000 followers\n")
+            print(f"Final score: {score}\n")
+
+if score > high_scores["score"]:
+    print("You've achieved a high score!")
+    high_scores["score"] = score
+    high_scores["name"] = input("Enter your name: ")
+    with open("db.json","w") as db:
+        json.dump(high_scores, db)
+    db.close()
+
