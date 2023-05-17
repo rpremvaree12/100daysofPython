3b883322661a10da13d166b88d19e3a4590a57df
rpremvaree12 <richard.premvaree@gmail.com>
2023-05-17 06:57:51 -0400

    order and drink made

--- 015coffeeMachine/main.py
+++ 015coffeeMachine/main.py
@@ -34,23 +34,60 @@ MENU = {
 resources = {
     "water": 300,
     "milk": 200,
-    "coffee": 100,
-    "money": 0
+    "coffee": 100
+}
+
+denom = {
+    "quarter": 0.25,
+    "dime": 0.10,
+    "nickel": 0.05,
+    "penny": 0.01
 }
 
 def report():
     for item in resources: 
-        if item == "water" or item == "milk" or item == "coffee":
+        if item == "water" or item == "milk":
             print(f"{item}: {resources[item]} ml")
-    return
-
-def process_coins():
+        elif item == "coffee":
+            print(f"{item}: {resources[item]} g")
+        elif item == "money":
+            print(f"{item}: ${resources[item]}")
     return
 
 def check_resources(bev):
-    return True
+    for r in resources:
+        if r in MENU[bev]["ingredients"] and (resources[r] - MENU[bev]["ingredients"][r]) < 0:
+            print(f"Not enough {r} to make {bev}")
+            return False
+        return True
 
-def make_coffee():
-    return
+def collect_money():
+    total = 0
+    for c in denom:
+        total += int(input(f"How many {c}s do you have? ")) * denom[c]
+    return total
 
-report()
+
+def make_drink(bev):
+    if check_resources(bev):
+        for r in resources:
+            if r in MENU[bev]["ingredients"]:
+                resources[r] -= MENU[bev]["ingredients"][r]
+        print(f"Here's your {bev}! Enjoy!")
+        return True
+    else:
+        return False
+
+operating = True
+
+while operating:
+    bev = input("Would you like an espresso, latte or cappuccino? ").lower()
+    if bev == "report":
+        report()
+    elif bev == "off":
+        operating = False
+    elif collect_money() - MENU[bev]["cost"] > 0:
+        make_drink(bev)
+        print("")
+    else:
+        print("Sorry, you don't have enough money.")
