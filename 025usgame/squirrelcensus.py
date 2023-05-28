76f8c40e87866f376f0f74577af956bfcc7d44de
rpremvaree12 <richard.premvaree@gmail.com>
2023-05-28 09:53:28 -0400

    squirrel count

--- 025usgame/squirrelcensus.py
+++ 025usgame/squirrelcensus.py
@@ -3,11 +3,14 @@ import pandas as pd
 df = pd.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
 
 # create a csv of squirrel count of fur color
-fur_dict = {}
+fur_dict = {
+    "Fur Color" : df['Primary Fur Color'].unique(),
+    "Count" : []
+}
 # print(df['Primary Fur Color'].unique())
-for i in df['Primary Fur Color'].unique():
-    # print(i)
-    fur_dict[i] = df[df['Primary Fur Color'] == "Gray"].count()
-# gray_fur = df[df['Primary Fur Color']=="Gray"].count()
-# print(fur_dict["Black"])
-print(fur_dict)
+for i in fur_dict["Fur Color"]:
+    fur_dict["Count"].append(len(df[df['Primary Fur Color']==i]))
+
+
+fur_color_df = pd.DataFrame(fur_dict)
+fur_color_df.to_csv("./squirrel_fur_count.csv")
