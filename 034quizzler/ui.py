41f815ea8c3b8f6bea37ff00f4ab8ebce34edb1b
rpremvaree12 <richard.premvaree@gmail.com>
2023-07-22 23:54:41 -0400

    quizzler ui

--- 034quizzler/ui.py
+++ 034quizzler/ui.py
@@ -1,2 +1,14 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
 
+class Quiz_UI:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg = THEME_COLOR)
+
+        
+
+
+        self.window.mainloop()
